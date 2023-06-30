9e0c447648376c41d0f2583894c11e0e1372ae1d
JC <jc.climentpardo@gmail.com>
2023-06-30 13:10:51 -0400

    tested extracttion and registration
    Signed-off-by: JC <jc.climentpardo@gmail.com>

--- cfg/preprocess_cfg.py
+++ cfg/preprocess_cfg.py
@@ -2,10 +2,11 @@ from pathlib import Path
 
 PROJ_DIR = Path("/home/jc053/GIT/mri-longitudinal-segmentation")
 # INPUT_DIR = PROJ_DIR / "data/test_data"
+# INPUT_DIR = Path("/mnt/93E8-0534/JuanCarlos/mri-long-segmentation/T2")
 INPUT_DIR = PROJ_DIR / "data/T2"
 OUPUT_DIR = PROJ_DIR / "data/output"
 
-REG_DIR = OUPUT_DIR / "T2W_reg"
+REG_DIR = OUPUT_DIR / "T2W_registration"
 BF_CORRECTION_DIR = OUPUT_DIR / "T2W_bf_correction"
 BRAIN_EXTRACTION_DIR = OUPUT_DIR / "T2W_brain_extraction"
 SEG_PRED_DIR = OUPUT_DIR / "seg_predictions"
@@ -13,9 +14,9 @@ SEG_PRED_DIR = OUPUT_DIR / "seg_predictions"
 TEMP_DIR = PROJ_DIR / "temp_dir"
 TEMP_IMG = TEMP_DIR / "temp_head.nii.gz"
 
-REGISTRATION = True
-EXTRACTION = False
-BF_CORRECTION = False
+REGISTRATION = False
+EXTRACTION = True
+BF_CORRECTION = False       #beware,there is a predefined BF in the registration process already
 
 
 LIMIT_LOADING = 5 
--- mri_preprocess_3d.py
+++ mri_preprocess_3d.py
@@ -34,15 +34,15 @@ def bf_correction(input_dir, output_dir):
         Images in nii.gz format
     """
 
-    for img_dir in sorted(glob.glob(input_dir + "/*.nii.gz")):
-        ID = img_dir.split("/")[-1].split(".")[0]
+    for img_path in sorted(glob.glob(input_dir + "/*.nii.gz")):
+        ID = img_path.split("/")[-1].split(".")[0]
         if ID[-1] == "k":
             continue
         else:
             print(ID)
-            img = sitk.ReadImage(img_dir, sitk.sitkFloat32)
+            img = sitk.ReadImage(img_path, sitk.sitkFloat32)
             img = sitk.N4BiasFieldCorrection(img)
-            ID = img_dir.split("/")[-1].split(".")[0]
+            ID = img_path.split("/")[-1].split(".")[0]
             fn = ID + "_bf_corrected.nii.gz"
             sitk.WriteImage(img, os.path.join(output_dir, fn))
     print("bias field correction complete!")
@@ -82,151 +82,149 @@ def registration(
         Exception if an error occurs.
     """
     print("Registering test data...")
-    # Actually read the data based on the user's selection.
+
     fixed_img = sitk.ReadImage(temp_img, sitk.sitkFloat32)
     problematic_IDs = []
+
     print("Preloading step...")
+
     for index, img_path in enumerate(tqdm(input_data)):
         try:
             _ = sitk.ReadImage(img_path, sitk.sitkFloat32)
         except Exception as e:
-            problematic_IDs.append(os.path.splitext(img_path)[0])
-    print(problematic_IDs)
-    count = 0
+            problematic_ID = os.path.splitext(img_path)[0]
+            problematic_IDs.append(problematic_ID)
+            print(f"Could not preload image {problematic_ID}. Error: {e}")
+            
+    print("Problematic IDs: ", problematic_IDs)
+
     random.shuffle(input_data)
-    for img_dir in tqdm(input_data):
+
+    for index, img_path in enumerate(tqdm(input_data)):
         ID = os.path.splitext(img_path)[0]
         if ID in problematic_IDs:
             print("problematic data!")
-        else:
-            # if "_mask" in ID:
-            #    continue
-            print(count)
-            print(ID)
-            try:
-                # pat_id = img_dir.split('/')[-1].split('.')[0]
-                # if "_mask.nii.gz" in img_dir:
-                #    continue
+            continue
 
-                # if os.path.exists(os.path.join(output_dir, str(pat_id) + '_0000.nii.gz')):
-                #     continue
-                # segmentation_loc = img_dir.replace(".nii.gz","_mask.nii.gz")
+        print(index)
+        print(ID)
+        try:
+            moving_img = sitk.ReadImage(img_path, sitk.sitkFloat32)
+            # print('moving image:', moving_image.shape)
 
-                # if not os.path.exists(segmentation_loc):
-                #    continue
+            # bias filed correction
+            moving_img = sitk.N4BiasFieldCorrection(moving_img)
 
-                count += 1
-                moving_img = sitk.ReadImage(img_dir, sitk.sitkFloat32)
-                # bias filed correction
-                moving_img = sitk.N4BiasFieldCorrection(moving_img)
+            # respace fixed img on z-direction
+            z_spacing = moving_img.GetSpacing()[2]
+            old_size = fixed_img.GetSize()
+            old_spacing = fixed_img.GetSpacing()
+            new_spacing = (
+                1,
+                1,
+                1,
+            )  # CHANGED FROM ORIGINAL WHERE Z_SPACING WAS MAINTAINED
+            new_size = [
+                int(round((old_size[0] * old_spacing[0]) / float(new_spacing[0]))),
+                int(round((old_size[1] * old_spacing[1]) / float(new_spacing[1]))),
+                int(round((old_size[2] * old_spacing[2]) / float(new_spacing[2]))),
+            ]
+            # new_size = [old_size[0], old_size[1], int(round((old_size[2] * 1) / float(z_spacing)))]
+            # new_size = [old_size[0], old_size[1], old_size[2]]
+            if interp_type == "linear":
+                interp_type = sitk.sitkLinear
+            elif interp_type == "bspline":
+                interp_type = sitk.sitkBSpline
+            elif interp_type == "nearest_neighbor":
+                interp_type = sitk.sitkNearestNeighbor
+            resample = sitk.ResampleImageFilter()
+            resample.SetOutputSpacing(new_spacing)
+            resample.SetSize(new_size)
+            resample.SetOutputOrigin(fixed_img.GetOrigin())
+            resample.SetOutputDirection(fixed_img.GetDirection())
+            resample.SetInterpolator(interp_type)
+            resample.SetDefaultPixelValue(fixed_img.GetPixelIDValue())
+            resample.SetOutputPixelType(sitk.sitkFloat32)
+            fixed_img = resample.Execute(fixed_img)
+            # print(fixed_img.shape)
+            transform = sitk.CenteredTransformInitializer(
+                fixed_img,
+                moving_img,
+                sitk.Euler3DTransform(),
+                sitk.CenteredTransformInitializerFilter.GEOMETRY,
+            )
+            # multi-resolution rigid registration using Mutual Information
+            registration_method = sitk.ImageRegistrationMethod()
+            registration_method.SetMetricAsMattesMutualInformation(
+                numberOfHistogramBins=50
+            )
+            registration_method.SetMetricSamplingStrategy(
+                registration_method.RANDOM
+            )
+            registration_method.SetMetricSamplingPercentage(0.01)
+            registration_method.SetInterpolator(sitk.sitkLinear)
+            registration_method.SetOptimizerAsGradientDescent(
+                learningRate=1.0,
+                numberOfIterations=100,
+                convergenceMinimumValue=1e-6,
+                convergenceWindowSize=10,
+            )
+            registration_method.SetOptimizerScalesFromPhysicalShift()
+            registration_method.SetShrinkFactorsPerLevel(shrinkFactors=[4, 2, 1])
+            registration_method.SetSmoothingSigmasPerLevel(
+                smoothingSigmas=[2, 1, 0]
+            )
+            registration_method.SmoothingSigmasAreSpecifiedInPhysicalUnitsOn()
+            registration_method.SetInitialTransform(transform)
+            final_transform = registration_method.Execute(fixed_img, moving_img)
 
-                # print('moving image:', moving_image.shape)
-                # respace fixed img on z-direction
-                z_spacing = moving_img.GetSpacing()[2]
-                old_size = fixed_img.GetSize()
-                old_spacing = fixed_img.GetSpacing()
-                new_spacing = (
-                    1,
-                    1,
-                    1,
-                )  # CHANGED FROM ORIGINAL WHERE Z_SPACING WAS MAINTAINED
-                new_size = [
-                    int(round((old_size[0] * old_spacing[0]) / float(new_spacing[0]))),
-                    int(round((old_size[1] * old_spacing[1]) / float(new_spacing[1]))),
-                    int(round((old_size[2] * old_spacing[2]) / float(new_spacing[2]))),
-                ]
-                # new_size = [old_size[0], old_size[1], int(round((old_size[2] * 1) / float(z_spacing)))]
-                # new_size = [old_size[0], old_size[1], old_size[2]]
-                if interp_type == "linear":
-                    interp_type = sitk.sitkLinear
-                elif interp_type == "bspline":
-                    interp_type = sitk.sitkBSpline
-                elif interp_type == "nearest_neighbor":
-                    interp_type = sitk.sitkNearestNeighbor
-                resample = sitk.ResampleImageFilter()
-                resample.SetOutputSpacing(new_spacing)
-                resample.SetSize(new_size)
-                resample.SetOutputOrigin(fixed_img.GetOrigin())
-                resample.SetOutputDirection(fixed_img.GetDirection())
-                resample.SetInterpolator(interp_type)
-                resample.SetDefaultPixelValue(fixed_img.GetPixelIDValue())
-                resample.SetOutputPixelType(sitk.sitkFloat32)
-                fixed_img = resample.Execute(fixed_img)
-                # print(fixed_img.shape)
-                transform = sitk.CenteredTransformInitializer(
-                    fixed_img,
-                    moving_img,
-                    sitk.Euler3DTransform(),
-                    sitk.CenteredTransformInitializerFilter.GEOMETRY,
-                )
-                # multi-resolution rigid registration using Mutual Information
-                registration_method = sitk.ImageRegistrationMethod()
-                registration_method.SetMetricAsMattesMutualInformation(
-                    numberOfHistogramBins=50
-                )
-                registration_method.SetMetricSamplingStrategy(
-                    registration_method.RANDOM
-                )
-                registration_method.SetMetricSamplingPercentage(0.01)
-                registration_method.SetInterpolator(sitk.sitkLinear)
-                registration_method.SetOptimizerAsGradientDescent(
-                    learningRate=1.0,
-                    numberOfIterations=100,
-                    convergenceMinimumValue=1e-6,
-                    convergenceWindowSize=10,
-                )
-                registration_method.SetOptimizerScalesFromPhysicalShift()
-                registration_method.SetShrinkFactorsPerLevel(shrinkFactors=[4, 2, 1])
-                registration_method.SetSmoothingSigmasPerLevel(
-                    smoothingSigmas=[2, 1, 0]
-                )
-                registration_method.SmoothingSigmasAreSpecifiedInPhysicalUnitsOn()
-                registration_method.SetInitialTransform(transform)
-                final_transform = registration_method.Execute(fixed_img, moving_img)
+            ## WRITE MODIFIED SCAN
+            moving_img_resampled = sitk.Resample(
+                moving_img,
+                fixed_img,
+                final_transform,
+                sitk.sitkLinear,
+                0.0,
+                moving_img.GetPixelID(),
+            )
 
-                ## WRITE MODIFIED SCAN
-                moving_img_resampled = sitk.Resample(
-                    moving_img,
-                    fixed_img,
-                    final_transform,
-                    sitk.sitkLinear,
-                    0.0,
-                    moving_img.GetPixelID(),
-                )
+            # get the file names
+            filename = os.path.basename(img_path)  # Get filename from the full path
+            filename_parts = os.path.splitext(filename)[0].split("_")  # Split filename without extension on underscores
+            patient_id = filename_parts[0]
+            scan_id = filename_parts[1]
+            new_filename = f"{patient_id}_{scan_id}_0000.nii.gz"
+            output_path = os.path.join(output_dir, new_filename)
+            sitk.WriteImage(moving_img_resampled, output_path)
+            
+            segmentation_loc = img_path.replace(".nii.gz", "_label.nii.gz")
+            if not os.path.isfile(segmentation_loc):
+                print(f"No corresponding label file for {img_path}")
+                continue
 
-                path_parts = os.path.normpath(img_dir).split(os.sep)
-                patient_id = path_parts[-3]
-                scan_id = path_parts[-2]
-                new_filename = f"{patient_id}_{scan_id}_0000.nii.gz"
-                output_path = os.path.join(output_dir, new_filename)
-                sitk.WriteImage(moving_img_resampled, output_path)
-                segmentation_loc = img_path.replace(".nii.gz", "_label.nii.gz")
-                if not os.path.isfile(segmentation_loc):
-                    print(f"No corresponding label file for {img_path}")
-                    continue
+            moving_label = sitk.ReadImage(segmentation_loc, sitk.sitkFloat32)
+            moving_label_resampled = sitk.Resample(
+                moving_label,
+                fixed_img,
+                final_transform,
+                sitk.sitkNearestNeighbor,
+                0.0,
+                moving_img.GetPixelID(),
+            )
+            if not os.path.exists(os.path.join(nnunet_dir, "labelsTs")):
+                os.makedirs(os.path.join(nnunet_dir, "labelsTs"))
+            sitk.WriteImage(
+                moving_label_resampled,
+                os.path.join(nnunet_dir, "labelsTs", f"{ID}" + ".nii.gz"),
+            )
 
-                moving_label = sitk.ReadImage(segmentation_loc, sitk.sitkFloat32)
-                moving_label_resampled = sitk.Resample(
-                    moving_label,
-                    fixed_img,
-                    final_transform,
-                    sitk.sitkNearestNeighbor,
-                    0.0,
-                    moving_img.GetPixelID(),
+            if save_tfm:
+                sitk.WriteTransform(
+                    final_transform, os.path.join(output_dir, f"{ID}" + "_T2.tfm")
                 )
-                if not os.path.exists(os.path.join(nnunet_dir, "labelsTs")):
-                    os.makedirs(os.path.join(nnunet_dir, "labelsTs"))
-                sitk.WriteImage(
-                    moving_label_resampled,
-                    os.path.join(nnunet_dir, "labelsTs", str(ID) + ".nii.gz"),
-                )
-
-                if save_tfm:
-                    sitk.WriteTransform(
-                        final_transform, os.path.join(output_dir, str(ID) + "_T2.tfm")
-                    )
-            except Exception as e:
-                print(e)
+        except Exception as e:
+            print(f"Error with image {ID}: {e}")
+    count = index + 1
     print("Registered", count, "scans.")
 
 
@@ -243,14 +241,15 @@ def get_image_files(base_dir):
 
 
 if __name__ == "__main__":
-    os.environ["CUDA_VISIBLE_DEVICES"] = "1"
+    os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
     register = REGISTRATION
     extraction = EXTRACTION
-    bf_correction = BF_CORRECTION
+    bias_field_correction = BF_CORRECTION
 
     input_data_dir = INPUT_DIR
     output_path = OUPUT_DIR
+    
     reg_dir = REG_DIR
     brain_dir = BRAIN_EXTRACTION_DIR
     bf_correction_dir = BF_CORRECTION_DIR
@@ -275,5 +274,5 @@ if __name__ == "__main__":
     if extraction:
         brain_extraction(input_dir=reg_dir, output_dir=brain_dir)
 
-    if bf_correction:
+    if bias_field_correction:
         bf_correction(input_dir=brain_dir, output_dir=bf_correction_dir)
