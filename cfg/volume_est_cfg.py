ab9cf3bae2283f84599655f392fca2736dec9550
JC <jc.climentpardo@gmail.com>
2023-07-06 17:01:15 -0400

    added age
    Signed-off-by: JC <jc.climentpardo@gmail.com>

--- cfg/volume_est_cfg.py
+++ cfg/volume_est_cfg.py
@@ -2,3 +2,5 @@ from pathlib import Path
 
 SEG_DIR = Path("/home/jc053/GIT/mri-longitudinal-segmentation/data/output/seg_predictions/")
 PLOTS_DIR = Path("/mnt/93E8-0534/JuanCarlos/mri-long-segmentation/T2/output/plots/")
+REDCAP_FILE = Path("/home/jc053/GIT/mri-longitudinal-segmentation/data/redcap/redcap_nifti_stats.csv")
+LIMIT_LOADING = 60
--- volume_estimation.py
+++ volume_estimation.py
@@ -5,14 +5,27 @@ from datetime import datetime
 from multiprocessing import Pool, cpu_count
 import numpy as np
 import matplotlib.dates as mdates
+import matplotlib.ticker as ticker
 import matplotlib.pyplot as plt
 import SimpleITK as sitk
+import pandas as pd
 
-from cfg.volume_est_cfg import SEG_DIR, PLOTS_DIR
+from cfg.volume_est_cfg import SEG_DIR, PLOTS_DIR, REDCAP_FILE, LIMIT_LOADING
 
 class VolumeEstimator:
-    def __init__(self, path):
+    def __init__(self, path, dob_file):
         self.path = path
+
+        # Process the .csv with clinical data
+        self.dob_df = pd.read_csv(dob_file, sep=',', encoding='UTF-8') 
+        self.dob_df = self.dob_df[self.dob_df['no ops cohort'] == 'NAN']
+        print(f"The length of the filtered dataset is: {len(self.dob_df)}")
+        if len(self.dob_df) != 60:
+            print("Warning: The length of the filtered dataset is not 60")
+        self.dob_df['Date of Birth'] = pd.to_datetime(self.dob_df['Date of Birth'], dayfirst=False)
+        print(self.dob_df['Date of Birth'].unique())
+        self.dob_df['BCH MRN'] = self.dob_df['BCH MRN'].astype(int)
+        
         self.volumes = defaultdict(list)
 
     @staticmethod
@@ -25,29 +38,67 @@ class VolumeEstimator:
         total_volume = num_voxels * voxel_volume
         return total_volume
 
-    def process_files(self):
+    def process_files(self, max_patients=None):
         file_paths = glob.glob(os.path.join(self.path, "*.nii.gz"))
 
-        with Pool(cpu_count()) as p:
-            results = p.map(self.estimate_volume, file_paths)
+        patient_ids = set()
+        for file_path in file_paths:
+            file_name = os.path.basename(file_path)
+            patient_id, _ = file_name.split("_")[0], file_name.split("_")[1]
+            patient_ids.add(patient_id)
 
-        for file_path, volume in zip(file_paths, results):
+        if max_patients is not None and max_patients < len(patient_ids):
+            patient_ids = list(patient_ids)[:max_patients]
+
+        filtered_file_paths = [fp for fp in file_paths if os.path.basename(fp).split("_")[0] in patient_ids]
+        filtered_df = self.dob_df[self.dob_df['BCH MRN'].astype(str).isin(patient_ids)]
+
+        print(len(filtered_df))
+
+        with Pool(cpu_count()) as p:
+            results = p.map(self.estimate_volume, filtered_file_paths)
+
+        for file_path, volume in zip(filtered_file_paths, results):
             file_name = os.path.basename(file_path)
             patient_id, date_str = file_name.split("_")[0], file_name.split("_")[1]
             date_str = date_str.replace(".nii.gz", "")
             date = datetime.strptime(date_str, "%Y%m%d")
-            volume = self.estimate_volume(file_path)
-            self.volumes[patient_id].append((date, volume))
+
+            if patient_id in filtered_df['BCH MRN'].astype(str).values:
+                dob = self.dob_df.loc[self.dob_df['BCH MRN'] == int(patient_id), 'Date of Birth'].iloc[0]
+                age = (date - dob).days / 365.25        
+                self.volumes[patient_id].append((date, volume, age))
 
     def plot_volumes(self, output_path):
         for patient_id, volumes in self.volumes.items():
             volumes.sort(key=lambda x: x[0])  # sort by date
-            dates, volumes = zip(*volumes)  # unzip to two lists
+            dates, volumes, ages = zip(*volumes)  # unzip to two lists
             os.makedirs(output_path, exist_ok=True)
 
-            plt.figure(figsize=(12, 8))
+            #plt.figure(figsize=(12, 8))
+            #plt.plot(dates, volumes, marker="o")
+            fig, ax1 = plt.subplots(figsize=(12, 8))
 
-            plt.plot(dates, volumes, marker="o")
+            color = 'tab:blue'
+            ax1.set_xlabel('Scan Date')
+            ax1.set_ylabel('Volume (mm³)', color=color)
+            ax1.plot(dates, volumes, color=color, marker="o")
+            ax1.tick_params(axis='y', labelcolor=color)
+            
+            ax1.xaxis.set_major_formatter(mdates.DateFormatter("%m/%d/%Y"))
+            ax1.set_xticks(dates)
+            ax1.set_xticklabels([dt.strftime('%m/%d/%Y') for dt in dates], rotation=90, fontsize=8)
+            ax1.xaxis.set_tick_params(pad=5)
+
+            ax2 = ax1.twiny()
+            ax2.xaxis.set_ticks_position('top') 
+            ax2.xaxis.set_label_position('top')
+            ax2.set_xlabel('Patient Age (Years)')
+            ax2.set_xlim(ax1.get_xlim())
+            date_nums = mdates.date2num(dates)
+            ax2.set_xticks(date_nums)
+            ax2.set_xticklabels([f"{age:.1f}" for age in ages])
+            ax2.xaxis.set_tick_params(labelsize=8)
 
             # volume changes
             volume_changes = [0]
@@ -58,10 +109,10 @@ class VolumeEstimator:
                     volume_change = np.nan  # or another value of your choice
                 volume_changes.append(volume_change)
 
-            for i, (date, volume, volume_change) in enumerate(
-                zip(dates, volumes, volume_changes)
+            for i, (date, volume, volume_change, age) in enumerate(
+                zip(dates, volumes, volume_changes, ages)
             ):
-                plt.text(
+                ax1.text(
                     date,
                     volume,
                     f"{volume_change:.2f}%",
@@ -70,16 +121,17 @@ class VolumeEstimator:
                     ha="left",
                 )
 
+
             plt.title(f"Patient ID: {patient_id}")
-            plt.xlabel("Date")
-            plt.ylabel("Volume (mm³)")
+            #plt.xlabel("Scan Date / Patient Age")
+            #plt.ylabel("Volume (mm³)")
 
             # Format x-axis to show dates
-            plt.gca().xaxis.set_major_formatter(mdates.DateFormatter("%m/%d/%Y"))
-            plt.xticks(ticks=dates, rotation=90, fontsize=8)
-            plt.gca().xaxis.set_tick_params(pad=5)
+            #plt.gca().xaxis.set_major_formatter(mdates.DateFormatter("%m/%d/%Y"))
+            #plt.xticks(ticks=dates, rotation=90, fontsize=8)
+            #plt.gca().xaxis.set_tick_params(pad=5)
 
-            plt.tight_layout()
+            fig.tight_layout()
 
             date_range = (
                 f"{min(dates).strftime('%Y%m%d')}_{max(dates).strftime('%Y%m%d')}"
@@ -91,8 +143,8 @@ class VolumeEstimator:
 
 
 if __name__ == "__main__":
-    ve = VolumeEstimator(SEG_DIR)
+    ve = VolumeEstimator(SEG_DIR, REDCAP_FILE)
     print("Getting prediction masks.")
-    ve.process_files()
+    ve.process_files(max_patients=LIMIT_LOADING)
     print("Saving data.")
     ve.plot_volumes(output_path=PLOTS_DIR)
