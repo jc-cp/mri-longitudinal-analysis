a4fd63b2c32f89e3f02dcf34211a351c6cb5b5c0
JC <jc.climentpardo@gmail.com>
2023-09-20 22:29:28 -0400

    /home/jc053/anaconda3/envs/mri-longitudinal-analysis/bin/python /home/jc053/GIT/mri-longitudinal-analysis/mri_longitudinal_analysis/utils/evaluation_t2w.py
    Signed-off-by: JC <jc.climentpardo@gmail.com>

--- mri_longitudinal_analysis/cfg/evaluation_cfg.py
+++ mri_longitudinal_analysis/cfg/evaluation_cfg.py
@@ -2,14 +2,14 @@ from pathlib import Path
 
 # overall path for reviewed no_ops T2 data
 DATA_FOLDER_60 = Path(
-    "/mnt/kannlab_rfa/JuanCarlos/mri-classification-sequences/curated_no_ops_60_cohort_filtered/T2"
+    "/home/jc053/GIT/mri-longitudinal-analysis/data/60_no_ops_cohort/T2/"
 )
 CSV_FILE_60 = DATA_FOLDER_60 / "annotations.csv"
 DATA_FOLDER_29 = Path(
-    "/mnt/kannlab_rfa/JuanCarlos/mri-classification-sequences/curated_no_ops_29_surgery_cohort_filtered/T2"
+    "/home/jc053/GIT/mri-longitudinal-analysis/data/29_no_ops_surgery_cohort/T2/"
 )
 CSV_FILE_29 = DATA_FOLDER_29 / "annotations.csv"
 
 # output images
-OUTPUT_DIR = Path("/home/jc053/GIT/mri-longitudinal-segmentation/data/output")
+OUTPUT_DIR = Path("/home/jc053/GIT/mri-longitudinal-analysis/data/output")
 OUT_FILE_PREFIX = OUTPUT_DIR / "output_evaluation_t2w"
--- mri_longitudinal_analysis/utils/evaluation_t2w.py
+++ mri_longitudinal_analysis/utils/evaluation_t2w.py
@@ -1,73 +1,113 @@
-import sys
-from pathlib import Path
+"""
+This script provides an evaluation framework for analyzing image quality ratings.
+It reads data from specified CSV files, processes and filters the data,
+and creates a histogram to visualize the distribution of quality ratings.
+"""
 
 import matplotlib.pyplot as plt
 import pandas as pd
 import seaborn as sns
 
-sys.path.append(sys.path.append(str(Path(__file__).resolve().parent.parent)))
-
 from cfg import evaluation_cfg
 
 
 class Evaluation:
+    """
+    A class to encapsulate the evaluation process for image quality.
+    """
+
     def __init__(self):
+        """
+        Initialize the evaluation object with files and other configurations.
+        """
         self.files = {
             "60": evaluation_cfg.CSV_FILE_60,
             "29": evaluation_cfg.CSV_FILE_29,
         }
         self.out_file_prefix = evaluation_cfg.OUT_FILE_PREFIX
 
-    def read_file(self, csv_file):
-        column_names = ["Image_Name", "Quality", "Comments"]
-        df = pd.read_csv(csv_file, names=column_names)
-        return df
+    def read_file(self, csv_file) -> pd.DataFrame:
+        """
+        Read a CSV file and return a DataFrame.
 
-    def parse_df(self, df):
+        Args:
+            csv_file (str): Path to the CSV file to be read.
+
+        Returns:
+            DataFrame: DataFrame containing image names, quality ratings, and comments.
+        """
+        column_names = ["Image_Name", "Quality", "Comments"]
+        d_f = pd.read_csv(csv_file, names=column_names)
+        return d_f
+
+    def parse_df(self, d_f) -> pd.DataFrame:
+        """
+        Parse and filter the DataFrame to compute various statistics.
+
+        Args:
+            d_f (DataFrame): Input DataFrame to be parsed.
+
+        Returns:
+            DataFrame: Filtered DataFrame containing only relevant data.
+        """
         # Compute some statistics
-        df["Quality"] = df["Quality"].astype(int)
-        num_images = df["Image_Name"].nunique()
-        avg_quality = df["Quality"].mean()
-        quality_dist = df["Quality"].value_counts()
+        d_f["Quality"] = d_f["Quality"].astype(int)
+        num_images = d_f["Image_Name"].nunique()
+        avg_quality = d_f["Quality"].mean()
+        quality_dist = d_f["Quality"].value_counts()
 
         print(f"Number of unique images: {num_images}")
         print(f"Average quality: {avg_quality:.2f}")
         print("\nQuality distribution:", quality_dist)
 
         # Get all unique comments
-        all_comments = df["Comments"].unique()
+        all_comments = d_f["Comments"].unique()
         print(f"All types of annotations: {all_comments}")
 
         # Filter comments for quality 5
-        quality5_comments = df[df["Quality"] == 5]["Comments"]
-        quality1_comments = df[df["Quality"] == 1]["Comments"]
-        print("\nComments for quality rating of 5:", quality5_comments.value_counts())
-        print("\nComments for quality rating of 1:", quality1_comments.value_counts())
-
+        quality5_comments = d_f[d_f["Quality"] == 5]["Comments"]
+        quality1_comments = d_f[d_f["Quality"] == 1]["Comments"]
+        print(
+            "\nComments for quality rating of 5:",
+            quality5_comments.value_counts(),
+        )
+        print(
+            "\nComments for quality rating of 1:",
+            quality1_comments.value_counts(),
+        )
         # Number of images with quality 1
-        quality1_no_comments = df[(df["Quality"] == 1) & (df["Comments"].isna())]
-        quality1_with_comments = df[(df["Quality"] == 1) & (df["Comments"].notna())]
+        quality1_no_comments = d_f[
+            (d_f["Quality"] == 1) & (d_f["Comments"].isna())
+        ]
+        quality1_with_comments = d_f[
+            (d_f["Quality"] == 1) & (d_f["Comments"].notna())
+        ]
         print(
-            f"\nNumber of images with quality rating of 1 and no comments: {len(quality1_no_comments)}"
-        )
-        print(
-            f"Number of images with quality rating of 1 and with comments: {len(quality1_with_comments)}"
+            f"\nImages with quality rating of 1 and no comments: {len(quality1_no_comments)}"
         )
 
-        # Number of images with quality 5
-        quality5_no_comments = df[(df["Quality"] == 5) & (df["Comments"].isna())]
-        quality5_with_comments = df[(df["Quality"] == 5) & (df["Comments"].notna())]
         print(
-            f"\nNumber of images with quality rating of 5 and no comments: {len(quality5_no_comments)}"
+            f"\nImages with quality rating of 1 and with comments: {len(quality1_with_comments)}"
         )
+        # Number of images with quality 5
+        quality5_no_comments = d_f[
+            (d_f["Quality"] == 5) & (d_f["Comments"].isna())
+        ]
+        quality5_with_comments = d_f[
+            (d_f["Quality"] == 5) & (d_f["Comments"].notna())
+        ]
+        print(
+            f"\nImages with quality rating of 5 and no comments: {len(quality5_no_comments)}"
+        )
+
         assert len(quality5_no_comments) == 0
         print(
-            f"Number of images with quality rating of 5 and with comments: {len(quality5_with_comments)}"
+            f"\nImages with quality rating of 5 and with comments: {len(quality5_with_comments)}"
         )
 
         # Compute some statistics
-        df["Quality"] = df["Quality"].astype(int)
-        df["Comments"] = df["Comments"].fillna("Valid Images")
+        d_f["Quality"] = d_f["Quality"].astype(int)
+        d_f["Comments"] = d_f["Comments"].fillna("Valid Images")
 
         # Define comment categories
         comment_categories = [
@@ -83,25 +123,32 @@ class Evaluation:
         ]
 
         # New column to hold the category
-        df["Category"] = df["Quality"].astype(str) + "-" + df["Comments"].map(str)
+        d_f["Category"] = (
+            d_f["Quality"].astype(str) + "-" + d_f["Comments"].map(str)
+        )
 
         # Only consider records with Quality as 1 or 5, and Comments in comment_categories
-        df_filtered = df[
-            df["Quality"].isin([1, 5]) & df["Quality"] == 1
-        ]  # df["Comments"].isin(comment_categories)]
+        df_filtered = d_f[d_f["Quality"].isin([1, 5]) & d_f["Quality"] == 1]
         return df_filtered
 
     def hist_plot(self, df_filtered, suffix):
+        """
+        Create and save a histogram plot based on the filtered DataFrame.
+
+        Args:
+            df_filtered (DataFrame): Filtered DataFrame for plotting.
+            suffix (str): Suffix to be appended to the output file name.
+        """
         prefix = str(self.out_file_prefix)
 
         # Histogram of the quality
         plt.figure(figsize=(12, 8))
         plt.title("Distribution of Quality Ratings")
-        ax = sns.countplot(data=df_filtered, x="Quality", hue="Comments")
+        a_x = sns.countplot(data=df_filtered, x="Quality", hue="Comments")
 
         # Iterate over the bars, and add a label for each
-        for p in ax.patches:
-            ax.annotate(
+        for p in a_x.patches:
+            a_x.annotate(
                 format(p.get_height(), ".0f"),
                 (p.get_x() + p.get_width() / 2.0, p.get_height()),
                 ha="center",
@@ -114,9 +161,12 @@ class Evaluation:
         plt.savefig(f"{prefix}_{suffix}_cohort.png")
 
     def main(self):
+        """
+        Main function to execute the evaluation workflow.
+        """
         for suffix, csv_file in self.files.items():
-            df = self.read_file(csv_file)
-            filtered_df = self.parse_df(df)
+            d_f = self.read_file(csv_file)
+            filtered_df = self.parse_df(d_f)
             self.hist_plot(filtered_df, suffix)
 
 
