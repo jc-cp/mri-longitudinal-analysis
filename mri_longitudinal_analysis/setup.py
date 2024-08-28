30a211bc8ab01d1bff15d491f01aeb8af8916c7e
JC <jc.climentpardo@gmail.com>
2024-08-28 11:00:23 -0400

    volume estimation reworked
    Signed-off-by: JC <jc.climentpardo@gmail.com>

--- mri_longitudinal_analysis/setup.py
+++ mri_longitudinal_analysis/setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mri_longitudinal_analysis",
-    version="0.2",
+    version="0.3",
     packages=find_packages(),
     install_requires=[
         # your dependencies here
